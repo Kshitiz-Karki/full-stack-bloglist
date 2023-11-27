7435371f798606011e5b7155c6c9fb8783c4ae62
Kshitiz Karki <karki.kshitiz.23@gmail.com>
2023-11-27 22:33:12 +0545

    update for e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -11,3 +11,13 @@ module.exports = defineConfig({
     }
   },
 })
+
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// })
